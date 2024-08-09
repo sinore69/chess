8cc74dd16ac96c99f54edc82ccc73394bd5809d5
saptarshi dutta <saptarshidutta599@gmail.com>
2024-08-09 23:05:33 +0530

    rook check 100%

--- client/src/functions/validator/king.ts
+++ client/src/functions/validator/king.ts
@@ -3,28 +3,28 @@ import { isUpperCase } from "../isuppercase";
 import { withinbounds } from "../withinbounds";
 
 export function isvalidkingmove(
-  rowindex: number,
-  colindex: number,
-  x: number,
-  y: number,
+  srcRow: number,
+  srcCol: number,
+  destRow: number,
+  destCol: number,
   piece: string,
   board: string[][],
   isUnderCheck: MutableRefObject<boolean>
 ) {
-  if (rowindex == x && colindex == y) {
+  if (srcRow == destRow && srcCol == destCol) {
     return false;
   }
-  if (isUpperCase(piece) === isUpperCase(board[x][y]) && board[x][y] !== "1") {
+  if (isUpperCase(piece) === isUpperCase(board[destRow][destCol]) && board[destRow][destCol] !== "1") {
     return false;
   }
   const row = [-1, -1, 0, 1, 1, 1, 0, -1, -1];
   const col = [0, 1, 1, 1, 0, -1, -1, -1];
   for (let i = 0; i < 8; i++) {
     if (
-      x == Number(rowindex) + row[i] &&
-      y == Number(colindex) + col[i] &&
-      withinbounds(x, y) &&
-      safeKingMove(board, x, y, piece)
+      destRow == Number(srcRow) + row[i] &&
+      destCol == Number(srcCol) + col[i] &&
+      withinbounds(destRow, destCol) &&
+      safeKingMove(board, destRow, destCol, piece)
     ) {
       return true;
     }
@@ -37,13 +37,15 @@ function safeKingMove(
   col: number,
   piece: string
 ) {
-  console.log(row,col)
   //downward
   for (let i = 1; i <= 7; i++) {
     if (!withinbounds(Number(row) + i, Number(col))) {
       break;
     }
-    if (board[Number(row) + i][Number(col)] == "1") {
+    if (
+      board[Number(row) + i][Number(col)] == "1" ||
+      board[Number(row) + i][Number(col)] == piece
+    ) {
       continue;
     }
     if (
@@ -62,7 +64,10 @@ function safeKingMove(
     if (!withinbounds(Number(row) - i, Number(col))) {
       break;
     }
-    if (board[Number(row) - i][Number(col)] == "1") {
+    if (
+      board[Number(row) - i][Number(col)] == "1" ||
+      board[Number(row) - i][Number(col)] == piece
+    ) {
       continue;
     }
     if (
@@ -78,11 +83,13 @@ function safeKingMove(
   }
   //left to right
   for (let i = 1; i <= 7; i++) {
-    console.log(Number(row), Number(col) + i)
     if (!withinbounds(Number(row), Number(col) + i)) {
       break;
     }
-    if (board[Number(row)][Number(col) + i] == "1") {
+    if (
+      board[Number(row)][Number(col) + i] == "1" ||
+      board[Number(row)][Number(col) + i] == piece
+    ) {
       continue;
     }
     if (
@@ -101,7 +108,10 @@ function safeKingMove(
     if (!withinbounds(Number(row), Number(col) - i)) {
       break;
     }
-    if (board[Number(row)][Number(col) - i] == "1") {
+    if (
+      board[Number(row)][Number(col) - i] == "1" ||
+      board[Number(row)][Number(col) - i] == piece
+    ) {
       continue;
     }
     if (
@@ -119,92 +129,92 @@ function safeKingMove(
 }
 
 export function iscastle(
-  rowindex: number,
-  colindex: number,
-  x: number,
-  y: number,
+  srcRow: number,
+  srcCol: number,
+  destRow: number,
+  destCol: number,
   board: string[][],
   color: string,
   wCastle: React.MutableRefObject<"" | "KQ" | "K" | "Q">,
   bCastle: React.MutableRefObject<"" | "kq" | "k" | "q">
 ) {
   if (color === "w") {
-    const king = board[rowindex][colindex] === "K" ? "K" : "k";
+    const king = board[srcRow][srcCol] === "K" ? "K" : "k";
     const rook = king === "K" ? "R" : "r";
     if (
-      rowindex == x &&
-      Number(colindex) + 2 == y &&
+      srcRow == destRow &&
+      Number(srcCol) + 2 == destCol &&
       (wCastle.current === "K" ||
         wCastle.current === "KQ") /* add fen string logic validation */
     ) {
       if (
-        board[rowindex][Number(colindex) + 1] === "1" &&
-        board[rowindex][Number(colindex) + 2] === "1"
+        board[srcRow][Number(srcCol) + 1] === "1" &&
+        board[srcRow][Number(srcCol) + 2] === "1"
       ) {
-        board[rowindex][colindex] = "1";
-        board[rowindex][Number(colindex) + 1] = rook;
-        board[rowindex][Number(colindex) + 2] = king;
-        board[rowindex][Number(colindex) + 3] = "1";
+        board[srcRow][srcCol] = "1";
+        board[srcRow][Number(srcCol) + 1] = rook;
+        board[srcRow][Number(srcCol) + 2] = king;
+        board[srcRow][Number(srcCol) + 3] = "1";
         wCastle.current = "";
         return board;
       }
     }
     if (
-      rowindex == x &&
-      (colindex - 2 == y || colindex - 3 == y) &&
+      srcRow == destRow &&
+      (srcCol - 2 == destCol || srcCol - 3 == destCol) &&
       (wCastle.current === "Q" ||
         wCastle.current === "KQ") /* add fen string logic validation */
     ) {
       if (
-        board[rowindex][colindex - 1] === "1" &&
-        board[rowindex][colindex - 2] === "1" &&
-        board[rowindex][colindex - 3] === "1"
+        board[srcRow][srcCol - 1] === "1" &&
+        board[srcRow][srcCol - 2] === "1" &&
+        board[srcRow][srcCol - 3] === "1"
       ) {
-        board[rowindex][colindex] = "1";
-        board[rowindex][colindex - 1] = rook;
-        board[rowindex][colindex - 2] = king;
-        board[rowindex][colindex - 4] = "1";
+        board[srcRow][srcCol] = "1";
+        board[srcRow][srcCol - 1] = rook;
+        board[srcRow][srcCol - 2] = king;
+        board[srcRow][srcCol - 4] = "1";
         wCastle.current = "";
         return board;
       }
     }
   }
   if (color === "b") {
-    const king = board[rowindex][colindex] === "K" ? "K" : "k";
+    const king = board[srcRow][srcCol] === "K" ? "K" : "k";
     const rook = king === "K" ? "R" : "r";
     if (
-      rowindex == x &&
-      (Number(colindex) + 2 == y || Number(colindex) + 3 == y) &&
+      srcRow == destRow &&
+      (Number(srcCol) + 2 == destCol || Number(srcCol) + 3 == destCol) &&
       (bCastle.current === "k" ||
         bCastle.current === "kq") /* add fen string logic validation */
     ) {
       if (
-        board[rowindex][Number(colindex) + 1] === "1" &&
-        board[rowindex][Number(colindex) + 2] === "1" &&
-        board[rowindex][Number(colindex) + 3] === "1"
+        board[srcRow][Number(srcCol) + 1] === "1" &&
+        board[srcRow][Number(srcCol) + 2] === "1" &&
+        board[srcRow][Number(srcCol) + 3] === "1"
       ) {
-        board[rowindex][colindex] = "1";
-        board[rowindex][Number(colindex) + 1] = rook;
-        board[rowindex][Number(colindex) + 2] = king;
-        board[rowindex][Number(colindex) + 4] = "1";
+        board[srcRow][srcCol] = "1";
+        board[srcRow][Number(srcCol) + 1] = rook;
+        board[srcRow][Number(srcCol) + 2] = king;
+        board[srcRow][Number(srcCol) + 4] = "1";
         bCastle.current = "";
         return board;
       }
     }
     if (
-      rowindex == x &&
-      colindex - 2 == y &&
+      srcRow == destRow &&
+      srcCol - 2 == destCol &&
       (bCastle.current === "k" ||
         bCastle.current === "kq") /* add fen string logic validation */
     ) {
       if (
-        board[rowindex][colindex - 1] === "1" &&
-        board[rowindex][colindex - 2] === "1"
+        board[srcRow][srcCol - 1] === "1" &&
+        board[srcRow][srcCol - 2] === "1"
       ) {
-        board[rowindex][colindex] = "1";
-        board[rowindex][colindex - 1] = rook;
-        board[rowindex][colindex - 2] = king;
-        board[rowindex][colindex - 3] = "1";
+        board[srcRow][srcCol] = "1";
+        board[srcRow][srcCol - 1] = rook;
+        board[srcRow][srcCol - 2] = king;
+        board[srcRow][srcCol - 3] = "1";
         bCastle.current = "";
         return board;
       }
