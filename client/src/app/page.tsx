93246a14460585a6d66d98f58c5d515e8f33aa9e
sinore69 <saptarshidutta599@gmail.com>
2025-06-05 00:05:33 +0530

    animation

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 import Board from "../components/Board";
 import Console from "../components/Console";
+import Header from "../components/Header";
+
 export default function Home() {
   return (
     <>
-      <div className="flex p-1 gap-x-4 bg-black min-h-screen overflow-auto items-center justify-center w-full">
-        <div className="flex max-w-full max-h-full flex-col sm:flex-row gap-x-4">
-          <Board movable={false} color={"w"}></Board>
-          <Console
-            mode={"bot"}
-            joinCode=""
-            joinLink=""
-          ></Console>
+      <div className="">
+        <div className="flex p-1 gap-x-4 bg-black min-h-screen overflow-hidden items-center justify-center w-full flex-col">
+          <Header></Header>
+          <div className="flex max-w-full max-h-full flex-col lg:flex-row gap-x-4 animate-fadeInUp justify-center">
+            <Board movable={false} color={"w"}></Board>
+            <Console
+              mode={"bot"}
+              joinCode=""
+              joinLink=""
+            ></Console>
+          </div>
         </div>
       </div>
     </>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -15,11 +15,16 @@ const config: Config = {
       },
       animation: {
         blink: "blink 1.5s infinite",
+        fadeInUp: 'fadeInUp 0.6s ease-out forwards',
       },
       keyframes: {
         blink: {
           "0%, 100%": { opacity: "1" },
           "50%": { opacity: "0.3" },
+        },
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
       },
     },
