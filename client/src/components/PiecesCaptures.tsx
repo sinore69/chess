6dde9533b06d965782e8ad0c7f7514d045ba1564
sinore69 <saptarshidutta599@gmail.com>
2025-06-17 22:58:15 +0530

    piece

--- client/src/components/PiecesCaptures.tsx
+++ client/src/components/PiecesCaptures.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function PiecesCaptures() {
   return (
-    <div className='h-full bg-blue-200'>PiecesCaptures</div>
+    <div className='h-full bg-blue-200 w-full'>PiecesCaptures</div>
   )
 }
